2d869da33ba7636b08f3c8a9c07c9336661d3f79
Danny Vail <danny_vail@outlook.com>
2019-02-16 15:45:45 -0500

    EXPOSE GET ENDPOINTS In CarController.java - Expose GET /cars/id/{id} to return the car based on the id - Expose GET /cars/year/{year} to return a list of all cars for that year model - Expose GET /cars/brand/{brand} to return a list of all cars of that brand - TODO: Log when GET /cars/brand/{brand} is accessed
    In CarRepository.java
    - Add named queries to find by year and find by brand

--- src/main/java/com/lambdaschool/restfulcars/car/CarController.java
+++ src/main/java/com/lambdaschool/restfulcars/car/CarController.java
@@ -1,10 +1,7 @@
 package com.lambdaschool.restfulcars.car;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,45 @@ public class CarController {
     this.RBMQ_TEMPLATE = rabbitTemplate;
   }
 
+  /**
+   * Find car by id.
+   *
+   * @param id  The car id
+   * @return    The car record matching the given id
+   */
+  @GetMapping("/id/{id}")
+  public Car findById(@PathVariable Long id) {
+    return CAR_REPO.findById(id).orElseThrow(() -> new CarNotFoundException(id));
+  }
+
+  /**
+   * Find cars by year.
+   *
+   * @param year  The car production year
+   * @return      A list of cars matching the given production year
+   */
+  @GetMapping("/year/{year}")
+  public List<Car> findByYear(@PathVariable int year) {
+    return CAR_REPO.findByYear(year);
+  }
+
+  /**
+   * Find cars by brand.
+   *
+   * @param brand The car brand
+   * @return      A list of cars matching the given car brand.
+   */
+  @GetMapping("/brand/{brand}")
+  public List<Car> findByBrand(@PathVariable String brand) {
+    return CAR_REPO.findByBrand(brand);
+  }
+
+  /**
+   * Load multiple sets of data from the request body.
+   *
+   * @param cars  A list of car data
+   * @return      The list of cars that was successfully saved
+   */
   @PostMapping("/upload")
   public List<Car> upload(@RequestBody List<Car> cars) {
     return CAR_REPO.saveAll(cars);
--- src/main/java/com/lambdaschool/restfulcars/car/CarRepository.java
+++ src/main/java/com/lambdaschool/restfulcars/car/CarRepository.java
@@ -2,8 +2,12 @@ package com.lambdaschool.restfulcars.car;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * CarRepository is a JPA database interface
  */
 public interface CarRepository extends JpaRepository<Car, Long> {
+  List<Car> findByYear(int year);
+  List<Car> findByBrand(String brand);
 }
